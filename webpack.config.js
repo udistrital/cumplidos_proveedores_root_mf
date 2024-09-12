4efb5e48eeda6f04f3f97f386a0ac4eddcfc255c
JuanCamiloMartinezLopez <juacmartinezl@correo.udistrital.edu.co>
2024-09-12 14:14:05 -0500

    feature: se ajusta la configuracion de webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-ts");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { DefinePlugin } = require("webpack");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
 
 module.exports = (webpackConfigEnv, argv) => {
   const orgName = "udistrital";
@@ -19,10 +22,29 @@ module.exports = (webpackConfigEnv, argv) => {
         inject: false,
         template: "src/index.ejs",
         templateParameters: {
+          isProd: webpackConfigEnv && webpackConfigEnv.isProd,
+          isDev: webpackConfigEnv && webpackConfigEnv.isDev,
           isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
           orgName,
         },
+        favicon: "./src/favicon.ico",
+      }),
+      new DefinePlugin({
+        isProd: webpackConfigEnv && webpackConfigEnv.isProd,
+        isDev: webpackConfigEnv && webpackConfigEnv.isDev,
+        isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
+      }),
+      new CopyWebpackPlugin({
+        patterns: [{ from: "src/assets/i18n", to: "assets/i18n" }],
       }),
     ],
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "dist"),
+      },
+      compress: true,
+      port: 4200,
+      historyApiFallback: true,
+    },
   });
 };
