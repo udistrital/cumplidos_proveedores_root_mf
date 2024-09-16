decc2523501782be26497fdaf1bc9e63f128f596
JuanCamiloMartinezLopez <juacmartinezl@correo.udistrital.edu.co>
2024-09-16 14:10:00 -0500

    feature: se agrega el client_id para pruebas y se ajusta el callback

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -9,13 +9,13 @@ export const environment = {
   TOKEN: {
     AUTORIZATION_URL:
       "https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize",
-    CLIENTE_ID: "e36v1MPQk2jbz9KM4SmKhk8Cyw0a",
+    CLIENTE_ID: "WxgYKINkTC6bNx8RmNrTKNDpUa4a",
     RESPONSE_TYPE: "id_token token",
     SCOPE: "openid email role documento",
-    REDIRECT_URL: "http://localhost:4200/",
+    REDIRECT_URL: "https://pruebascumplidosprovmf.portaloas.udistrital.edu.co",
     SIGN_OUT_URL:
       "https://autenticacion.portaloas.udistrital.edu.co/oidc/logout",
-    SIGN_OUT_REDIRECT_URL: "http://localhost:4200/",
+    SIGN_OUT_REDIRECT_URL: "https://pruebascumplidosprovmf.portaloas.udistrital.edu.co",
     AUTENTICACION_MID:
       "https://autenticacion.portaloas.udistrital.edu.co/apioas/autenticacion_mid/v1/token/userRol",
   },
@@ -23,6 +23,6 @@ export const environment = {
     "@udistrital/root-config": "//https://pruebascumplidosprovmf.portaloas.udistrital.edu.co/udistrital-root-config.js",
     "@udistrital/core-mf":
       "https://pruebascoreclientes.portaloas.udistrital.edu.co/main.js",
-    "@udistrital/revision-cumplido-prov-mf": "//localhost:4202/main.js",
+    "@udistrital/revision-cumplido-prov-mf": "https://pruebasrevisoncumplidoprov.portaloas.udistrital.edu.co/main.js",
   },
 };
